89e8ea1e3fe4f7a0c9dba6a1f652d677d509ae23
milot <milot.shala@gmail.com>
2016-09-12 09:34:49 +0200

    Swift 3 compatibility

--- AlteranLocalization/Alteran/Alteran.swift
+++ AlteranLocalization/Alteran/Alteran.swift
@@ -8,18 +8,18 @@
 
 import Foundation
 
-public class Alteran: NSObject {
+open class Alteran: NSObject {
 	
 	/*
 		Returns currently available languages. Languages must be added in the base bundle of the
 	  application in order for this to return them accordingly.
 	  - excludeBase parameter is used to exclude the Base language altogether.
   */
-	public class func availableLanguages(excludeBase: Bool = false) -> [String] {
-		var availableLanguages = NSBundle.mainBundle().localizations
+	open class func availableLanguages(_ excludeBase: Bool = false) -> [String] {
+		var availableLanguages = Bundle.main.localizations
 		
-		if let baseIndex = availableLanguages.indexOf("Base") where excludeBase == true {
-			availableLanguages.removeAtIndex(baseIndex)
+		if let baseIndex = availableLanguages.index(of: "Base") , excludeBase == true {
+			availableLanguages.remove(at: baseIndex)
 		}
 		
 		return availableLanguages
@@ -29,10 +29,10 @@ public class Alteran: NSObject {
 		Returns the default language, remember this can be default to English if no default language is
 		being returned.
 	*/
-	public class func defaultLanguage() -> String {
+	open class func defaultLanguage() -> String {
 		var defaultLanguage = String()
 		
-		guard let preferredLanguage = NSBundle.mainBundle().preferredLocalizations.first else {
+		guard let preferredLanguage = Bundle.main.preferredLocalizations.first else {
 			return MILDefaultLanguage
 		}
 		
@@ -50,8 +50,8 @@ public class Alteran: NSObject {
 	/*
 		Returns current selected language, if no language is selected returns the default language.
 	*/
-	public class func currentLanguage() -> String {
-		if let currentLanguage = NSUserDefaults.standardUserDefaults().objectForKey(MILCurrentLanguage) as? String {
+	open class func currentLanguage() -> String {
+		if let currentLanguage = UserDefaults.standard.object(forKey: MILCurrentLanguage) as? String {
 			return currentLanguage
 		}
 		return defaultLanguage()
@@ -60,29 +60,29 @@ public class Alteran: NSObject {
 	/*
 		Sets the current language in Swift 3 friendly format. setCurrent(language: "en")
 	*/
-	public class func setCurrent(language language: String) {
+	open class func setCurrent(language: String) {
 		let selectedLanguage = availableLanguages().contains(language) ? language : defaultLanguage()
 		if selectedLanguage != currentLanguage() {
-			NSUserDefaults.standardUserDefaults().setObject(selectedLanguage, forKey: MILCurrentLanguage)
-			NSUserDefaults.standardUserDefaults().synchronize()
-			NSNotificationCenter.defaultCenter().postNotificationName(MILLanguageChangeNotification, object: nil)
+			UserDefaults.standard.set(selectedLanguage, forKey: MILCurrentLanguage)
+			UserDefaults.standard.synchronize()
+			NotificationCenter.default.post(name: Notification.Name(rawValue: MILLanguageChangeNotification), object: nil)
 		}
 	}
 	
 	/*
 		Resets it to default language
 	*/
-	public class func resetCurrentLanguageToDefault() {
+	open class func resetCurrentLanguageToDefault() {
 		setCurrent(language: self.defaultLanguage())
 	}
 	
 	/*
 		Based on the current locale, returns the display name for the particular language.
 	*/
-	public class func displayNameFor(language language: String) -> String {
-		let locale = NSLocale(localeIdentifier: currentLanguage())
+	open class func displayNameFor(language: String) -> String {
+		let locale = Locale(identifier: currentLanguage())
 		
-		if let displayName = locale.displayNameForKey(NSLocaleLanguageCode, value: language) {
+		if let displayName = (locale as NSLocale).displayName(forKey: NSLocale.Key.languageCode, value: language) {
 			return displayName
 		}
 		return String()
